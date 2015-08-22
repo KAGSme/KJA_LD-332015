267891cd385f38710baa1cdfd24f13424b7be877
Kieran <kags1995@hotmail.co.uk>
2015-08-22 19:01:20 +0100

    Camera
    Move cursor to edges to move camera,
    use scroll wheel to zoom in and out,
    press space bar to reset camera onto player

---
+++ KJA_LD33UnityProject/Assets/My Assets/Scripts/CameraControlScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraControlScript : MonoBehaviour {
+
+    Transform playerPos;
+    public float cameraSpeed = 5;
+    public float zoomSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+	}
+
+    void Update()
+    {
+        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollDelta > 0) { Camera.main.orthographicSize -= scrollDelta * zoomSpeed; }
+        if (scrollDelta < 0) { Camera.main.orthographicSize -= scrollDelta * zoomSpeed; }
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 10, 80);
+
+        if (Input.mousePosition.x <= 0)
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.x >= Screen.width)
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y <= 0)
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.mousePosition.y >= Screen.height)
+        {
+            transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButton("CameraReset"))
+        {
+           transform.position = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+        }
+    }
+}
