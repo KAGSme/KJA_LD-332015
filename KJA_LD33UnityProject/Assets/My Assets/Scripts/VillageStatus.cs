050ad49d641db9ff221a0b4d46ee8cbc4a997dfb
Kieran <kags1995@hotmail.co.uk>
2015-08-24 17:40:15 +0100

    village status

---
+++ KJA_LD33UnityProject/Assets/My Assets/Scripts/VillageStatus.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillageStatus : MonoBehaviour {
+
+    [HideInInspector]
+    public static VillageStatus vStatus = null;
+
+    int villagerCount = 0;
+    int deathCount = 0;
+
+    public int VillagerCount
+    {
+        get { return villagerCount; }
+    }
+
+    public int IncreaseDeatchCount
+    {
+        set
+        {
+            deathCount += value;
+            if (deathCount == villagerCount) { 
+                //fail state
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if (vStatus != null) Destroy(this.gameObject);
+        else vStatus = this;
+
+        var villagers = FindObjectsOfType<CharMotor>();
+        foreach (var v in villagers)
+        {
+            if (v.gameObject.layer == LayerMask.NameToLayer("villager"))
+            {
+                villagerCount++;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
